85ed24e2c4a8e6d09aff03fba164b492ae62e445
saleh Alobaylan <Salehwleed1@gmaile.com>
2024-07-01 10:54:22 +0300

    Adding the images lab

---
+++ NodeJS_Backend/buildingAserver/MyDisplayImage/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
